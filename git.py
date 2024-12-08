1b4b0c3b4a7afe38101c48992bd9ef605c869d55
Enternama <aleksrom2023@gmail.com>
2024-12-08 18:23:42 +0300

    123321

--- git.py
+++ git.py
@@ -1,6 +1,36 @@
-def main():
-    print('My first git program')
+import sys
+import random
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt6.QtGui import QPainter, QColor, QPen
+from PyQt6.QtCore import Qt
+from PyQt6 import uic
 
 
-if __name__ == '__main__':
-    main()
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+
+        self.circles = []
+        self.pushButton.clicked.connect(self.action)
+
+    def action(self):
+        diameter = random.randint(10, 50)
+        x = random.randint(10, self.width() - diameter - 10)
+        y = random.randint(10, self.height() - diameter - 10)
+        self.circles.append((x, y, diameter))
+        self.update()
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        pen = QPen(QColor("yellow"), 2)
+        painter.setPen(pen)
+        for x, y, diameter in self.circles:
+            painter.drawEllipse(x, y, diameter, diameter)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
